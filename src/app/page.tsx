564bf3db47e0b3640c358c97d1a846058dcdfc9c
Tristan Yang <yanggc888@163.com>
2023-10-27 11:22:35 +0800

    chore: updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,13 +27,13 @@ export default function IndexPage() {
               return (
                 <div
                   key={idx}
-                  className="relative flex flex-col gap-2 rounded bg-slate-100 p-2"
+                  className="relative flex flex-col gap-2 rounded bg-slate-100 p-2 dark:bg-slate-900"
                 >
-                  <div className="flex-1 whitespace-pre bg-green-100 p-2">
+                  <div className="flex-1 whitespace-pre bg-green-100 p-2 dark:bg-green-900">
                     {m.text}
                   </div>
                   {m.mentions.length > 0 && (
-                    <span className="bg-fuchsia-200 p-2">
+                    <span className="bg-fuchsia-200 p-2 dark:bg-fuchsia-800">
                       mentions:{' '}
                       <em className="text-xs not-italic">
                         {m.mentions.map((m) => m).join(',')}
